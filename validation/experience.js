d4fdf88b504b77c6748e42114cce3f9195caed62
Faraz Hayder <122741190+FarazHayder@users.noreply.github.com>
2025-03-20 22:11:46 +0500

    Added Experience & Education Routes

---
+++ validation/experience.js
@@ -0,0 +1,29 @@
+// This can be done in the profile.js, but that would just make that file messy, therefore, we make a separate file for this.
+
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateExperienceInput(data) {
+  let errors = {};
+
+  data.title = !isEmpty(data.title) ? data.title : "";
+  data.company = !isEmpty(data.company) ? data.company : "";
+  data.from = !isEmpty(data.from) ? data.from : "";
+
+  if (Validator.isEmpty(data.title)) {
+    errors.title = "Job title field is required";
+  }
+
+  if (Validator.isEmpty(data.company)) {
+    errors.company = "Company field is required";
+  }
+
+  if (Validator.isEmpty(data.from)) {
+    errors.from = "From date field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
